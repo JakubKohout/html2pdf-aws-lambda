2327bfb3cd093d0ad2a5db1bef42a4910d2d9274
Patrik Votocek <patrik@votocek.cz>
2015-11-25 02:50:42 +0100

    Add orientation and pageSize options

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ var streamBuffers = require('stream-buffers');
 process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/bin';
 
 exports.handler = function(event, context) {
+	var options = {
+		pageSize: event.pageSize || 'A4', // A5/A4/A3
+		orientation: event.orientation || 'Portrait' // Portrait/Landscape
+	};
+	
 	if (event.html) {
 		var stream = new streamBuffers.WritableStreamBuffer();
 		
-		wkhtmltopdf(event.html, { pageSize: 'A4' }, function(code, signal) {
+		wkhtmltopdf(event.html, options, function(code, signal) {
 			if (code !== null) {
 				console.log('Error: ' + code);
 				context.fail(code, {});
